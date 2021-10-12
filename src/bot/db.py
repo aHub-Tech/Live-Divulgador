f6b417813d997f310fe33655be26cd80d1dae0a5
Lucas Jantsch Guedes <llucas.jguedes@gmail.com>
2021-10-11 21:38:47 -0300

    Removendo partes desnecessárias em db.py

--- src/bot/db.py
+++ src/bot/db.py
@@ -1,36 +1,17 @@
-import datetime
 import os
 from urllib.parse import quote_plus
 
 from psycopg2 import OperationalError as PostgreSqlError
-from sqlalchemy import MetaData, create_engine, inspect
+from sqlalchemy import MetaData, create_engine
 from sqlalchemy.exc import OperationalError as SqlAlchemyError
-from sqlalchemy_utils import create_database, database_exists
 
 # Credenciais da Base de Dados
 user_db = os.getenv('user_db')
 passwd_db = os.getenv('passwd_db')
 
 # Variaveis opcionais
-host_db = os.getenv('host_db')
-port_db = os.getenv('port_db')
-
-# Tempo, em segundos, de espera até poder
-# ser divulgado novamente
-timeout = 900
-
-if user_db is None or user_db == '':
-    raise ValueError('user_db não encontrado')
-
-
-if passwd_db is None or passwd_db == '':
-    raise ValueError('passwd_db não encontrado')
-
-if host_db is None or host_db == '':
-    host_db = "db"
-
-if port_db is None or port_db == '':
-    port_db = "5432"
+host_db = "db"
+port_db = "5432"
 
 try:
     # Caso a password tenha caracteres especiais
@@ -39,19 +20,6 @@ try:
         "postgresql://{}:{}@{}:{}/streamers".format(
             user_db, quote_plus(passwd_db), host_db, port_db
         )
-    )
-
-    # Cria banco de dados caso não exista
-    if not database_exists(engine.url):
-        create_database(engine.url)
-
-    # Cria tabela 'livecoders' caso não exista
-    engine.execute(
-        "CREATE TABLE IF NOT EXISTS livecoders (Nome varchar(50), Id integer,\
-        Twitch varchar(150), Twitter varchar(50), OnStream boolean, Print boolean,\
-        Tipo varchar(5), Hashtags varchar(300),\
-        Timer timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP(0),\
-        Timedout boolean NOT NULL DEFAULT FALSE)"
     )
 
     # Guardar objeto da table livecoders
@@ -68,7 +36,6 @@ except PostgreSqlError as e:
 except SqlAlchemyError as e:
     print(e)
     exit()
-
 
 def return_streamer_info():
     """
@@ -97,7 +64,7 @@ def insert_streamers(streamers):
     Insere novos streamers na DB
     """
 
-    for index, row in streamers.iterrows():
+    for _index, row in streamers.iterrows():
 
         ins = livecoders.insert().values(
             nome=row["Nome"],
@@ -129,7 +96,6 @@ def insert_on_stream(idt, value):
 
     return
 
-
 def update_name(idt, name, twitch):
     """
     Função que atualizar o nome e o link com base no id
@@ -144,7 +110,6 @@ def update_name(idt, name, twitch):
 
     return
 
-
 def delete_streamer(idt):
     """
     Função que elimina streamer da DB com base no id
@@ -154,7 +119,6 @@ def delete_streamer(idt):
     engine.execute(delete)
 
     return
-
 
 def set_timedout(idt, bool):
     """
