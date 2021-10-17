e61b5f462c60b2e253207dbbe7937293f856cb1c
vcwild <vcwildm@gmail.com>
2021-10-17 03:51:05 -0300

    feat: update twitter client

--- src/bot/twitter/client.py
+++ src/bot/twitter/client.py
@@ -1,66 +1,51 @@
-import logging
-from dataclasses import dataclass
-from yaml import load, FullLoader
+from logging import getLogger
 from tweepy import API, OAuthHandler
 
-logger = logging.getLogger(__name__)
+from src.bot.twitter.generics import ClientKeys, TweetMetadata
+from src.bot.twitter.message import Message
 
-@dataclass
-class IClientKeys:
-    consumer_key: str
-    consumer_secret: str
-    access_token: str
-    access_token_secret: str
-
-@dataclass
-class ITweetMetadata:
-    twitter_display_name: str
-    twitch_channel: str
-    twitch_stream_title: str
-    tags: list[str]
-    thumbnail: str
+logger = getLogger(__name__)
 
 
 class TwitterClient:
-    def __init__(self, client_keys: IClientKeys) -> None:
-        self.api = __class__.get_api(
-            client_keys.consumer_key,
-            client_keys.consumer_secret,
-            client_keys.access_token,
-            client_keys.access_token_secret,
-        )
+    def __init__(self, client_keys: ClientKeys) -> None:
+        self._api = __class__.get_api(client_keys)
 
     @staticmethod
-    def get_api(
-        consumer_key,
-        consumer_secret,
-        access_token,
-        access_token_secret,
-    ):
-        auth = OAuthHandler(consumer_key, consumer_secret)
-        auth.set_access_token(access_token, access_token_secret)
+    def fetch_api(client_keys: ClientKeys) -> API:
+        auth = OAuthHandler(
+            client_keys.consumer_key, client_keys.consumer_secret
+        )
+        auth.set_access_token(
+            client_keys.access_token, client_keys.access_token_secret
+        )
 
         api = API(auth)
 
         try:
             api.verify_credentials()
         except Exception as err:
-            logger.error(f"An error has occurred: {err}")
+            logger.error(f"{__class__.__name__} error: {err}")
+            raise err
 
         return api
 
-    def send_tweet(self, tweet_metadata: ITweetMetadata) -> None:
-        with open("message.yml", "r") as file:
-            stream = file.read()
-            data = load(stream, Loader=FullLoader)
+    @staticmethod
+    def get_api(client_keys: ClientKeys):
+        api = __class__.fetch_api(client_keys)
 
-        message = data["message"].format(
+        return api
+
+    def send_tweet(self, tweet_metadata: TweetMetadata) -> None:
+        message = Message.PROTOTYPE.value.format(
             tweet_metadata.twitter_display_name,
             tweet_metadata.twitch_stream_title,
             tweet_metadata.twitch_channel,
             tweet_metadata.tags,
+            tweet_metadata.thumbnail,
         )
 
-        self.api.update_status(
-            status=message, attachment_url=tweet_metadata.thumbnail
-        )
+        try:
+            self._api.update_status(status=message)
+        except Exception as err:
+            logger.error(f"{self.__class__.__name__} error: {err}")
