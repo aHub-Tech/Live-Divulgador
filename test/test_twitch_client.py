b9f832435ae0632f2f438013bee4d295c09ae853
vcwild <vcwildm@gmail.com>
2021-10-17 00:47:05 -0300

    feat: update tests

--- test/test_twitch_client.py
+++ test/test_twitch_client.py
@@ -3,29 +3,30 @@ from pytest import fixture
 from dotenv import load_dotenv
 from os import getenv
 from pytest_schema import schema
-from random import randint
+
 
 load_dotenv()
 
-TWITCH_CLIENT_ID=getenv("CLIENT_ID")
-TWITCH_CLIENT_SECRET=getenv("CLIENT_SECRET")
+TWITCH_CLIENT_ID = getenv("CLIENT_ID")
+
+TWITCH_CLIENT_SECRET = getenv("CLIENT_SECRET")
 
 STREAMER_SCHEMA = {
-        'id': str,
-        'user_id': str,
-        'user_login': str,
-        'user_name': str,
-        'game_id': str,
-        'game_name': str,
-        'type': str,
-        'title': str,
-        'viewer_count': int,
-        'started_at': str,
-        'language': str,
-        'thumbnail_url': str,
-        'tag_ids': list[str],
-        'is_mature': bool
-    }
+    'id': str,
+    'user_id': str,
+    'user_login': str,
+    'user_name': str,
+    'game_id': str,
+    'game_name': str,
+    'type': str,
+    'title': str,
+    'viewer_count': int,
+    'started_at': str,
+    'language': str,
+    'thumbnail_url': str,
+    'tag_ids': list[str],
+    'is_mature': bool,
+}
 
 
 @fixture
@@ -43,14 +44,13 @@ def test_get_streamer_id(twitch_client):
 
 
 def test_get_streams(twitch_client):
-    response = twitch_client.get_streams()[0] # session.query(Streamer).select(Streamer.twitch_id).all()
-
+    response = twitch_client.get_random_streams()[0]
     assert response == schema(STREAMER_SCHEMA)
 
-
-def test_get_stream(twitch_client):
-    user_id = twitch_client.get_streams()[0]["user_id"]
-
-    response = twitch_client.get_stream(user_id)
-
-    assert response == schema(STREAMER_SCHEMA)
+def test_get_stream_should_return_list_of_streams_if_stream_exists_else_empty_list(twitch_client):
+    user_id = twitch_client.get_random_streams()
+    user_ids = list(map(lambda x: x['user_id'], user_id))
+    response = twitch_client.get_streams(user_ids)
+    if response == []:
+        assert True
+    assert response == schema(list[STREAMER_SCHEMA])
