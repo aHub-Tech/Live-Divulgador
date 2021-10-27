c05b65a9f776c83997dd9497fe8eea14fd781b8b
vcwild <vcwildm@gmail.com>
2021-10-27 02:47:07 -0300

    feat: create new streamers service tests

--- test/service/test_streamers_service.py
+++ test/service/test_streamers_service.py
@@ -1,34 +1,31 @@
 from src.bot.service.streamers_service import StreamersService
-
 from src.bot.database.engine import Session
-
 from src.bot.database.entities.streamer import Streamer
-
 from src.bot.database.entities.streamer import Base, Streamer
-
 from src.bot.service.streamers_service import StreamersService
+from src.bot.database.engine import engine
+from pytest import fixture, mark
 
 
-# Base.metadata.create_all(engine)
-
-
-def test_create_streamer():
-    streamer = Streamer(
-        twitch_id=23452356,
-        twitter_id=234232345,
-        name="Dornelles"
-    )
-
+@fixture
+def created_streamer(twitch_id):
+    streamer = Streamer(twitch_id=twitch_id, twitter_id=8250, name="Radhy")
     StreamersService.create_streamer(streamer)
 
-    response = StreamersService.get_streamer(streamer.id)
+    yield streamer
 
-    assert streamer.id == response.id
+    StreamersService.delete_streamer(twitch_id)
 
 
-def test_database_session():
-    session = Session()
+@mark.parametrize("twitch_id", [89037])
+def test_get_streamer(created_streamer, twitch_id):
+
+    response = StreamersService.get_streamer(twitch_id)
+
+    assert twitch_id == response[0].twitch_id
 
 
 def test_database_get_streamers():
-    ...
+    streamers = StreamersService.get_streamers()
+
+    assert len(streamers) > 0
