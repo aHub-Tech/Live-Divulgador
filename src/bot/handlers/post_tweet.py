c9dd7111feaeed0c49c3e6076e5e35008f39d187
vcwild <vcwildm@gmail.com>
2021-10-29 21:53:23 -0300

    feat: create post tweet routine

--- src/bot/handlers/post_tweet.py
+++ src/bot/handlers/post_tweet.py
@@ -1,8 +1,12 @@
-from dotenv import load_dotenv
+from collections import deque
+from logging import getLogger
 from os import getenv
 
-from bot.twitter.client import ClientKeys, TweetMetadata, TwitterClient
+from dotenv import load_dotenv
+
 from bot.handlers.verify_online_streamers import VerifyOnlineStreamers
+from bot.helpers.timeout import TimeoutValue
+from bot.twitter.client import ClientKeys, TweetMetadata, TwitterClient
 
 load_dotenv()
 
@@ -15,7 +19,6 @@ ACCESS_TOKEN_SECRET_A = getenv("ACCESS_TOKEN_SECRET_A")
 TWITCH_CLIENT_ID = getenv("CLIENT_ID")
 TWITCH_CLIENT_SECRET = getenv("CLIENT_SECRET")
 
-from logging import getLogger
 
 logger = getLogger(__name__)
 
@@ -26,6 +29,10 @@ class PostTweet:
     )
 
     twitter_client = TwitterClient(client_keys)
+
+    cache: dict = {
+        "tweeted": [],
+    }
 
     @classmethod
     def handle(cls) -> None:
@@ -38,14 +45,14 @@ class PostTweet:
             raise e
 
     @classmethod
-    def set_tweet_metadata(cls, data: dict):
+    def generate_tweet_metadata(cls, data: dict) -> TweetMetadata:
         user_name = data["user_name"]
         live_title = data["title"]
         twitch_channel = f"https://twitch.tv/{data['user_name']}"
         tags = "#Twitch #live"
         thumbnail = ""
 
-        cls.tweet_metadata = TweetMetadata(
+        tweet_metadata = TweetMetadata(
             twitter_display_name=user_name,
             twitch_channel=twitch_channel,
             twitch_stream_title=live_title,
@@ -53,8 +60,50 @@ class PostTweet:
             thumbnail=thumbnail,
         )
 
+        return tweet_metadata
+
+    @classmethod
+    def update_tweeted(cls, data: dict) -> None:
+        value = TimeoutValue(data["user_id"], 3600)
+        cls.cache["tweeted"].append(value)
+
+    @classmethod
+    def ensure_cache_not_empty(cls):
+        if cls.cache["tweeted"] == []:
+            value = TimeoutValue("0", 15)
+            cls.cache["tweeted"].append(value)
+
+    @classmethod
+    def refresh_tweeted_cache(cls):
+        cls.ensure_cache_not_empty()
+
+        for element in cls.cache["tweeted"]:
+            if element.value == None:
+                cls.cache["tweeted"].remove(element)
+
+    @classmethod
+    def remove_cached_from_tweet_list(cls, data):
+        cls.refresh_tweeted_cache()
+
+        tweeted = cls.cache["tweeted"]
+        tweeted_ids = [element.value for element in tweeted]
+
+        online_streamer_not_cached = (
+            lambda user: user if user["user_id"] not in tweeted_ids else None
+        )
+
+        online_streamer_to_tweet = list(filter(online_streamer_not_cached, data))
+
+        return online_streamer_to_tweet
+
     @classmethod
     def tweet(cls, live_list: list[dict]) -> None:
-        for live in live_list:
-            cls.set_tweet_metadata(live)
-            cls.twitter_client.send_tweet(cls.tweet_metadata)
+        not_cached_live_list = cls.remove_cached_from_tweet_list(live_list)
+
+        live_tweet_metadata: list[TweetMetadata] = list(
+            map(cls.generate_tweet_metadata, not_cached_live_list)
+        )
+
+        deque(map(cls.twitter_client.send_tweet, live_tweet_metadata))
+
+        deque(map(cls.update_tweeted, not_cached_live_list))
