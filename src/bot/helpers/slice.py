05a50e124a5aae0c6723c8f9ec85c915498597d3
vcwild <vcwildm@gmail.com>
2021-10-29 21:53:47 -0300

    feat: create queue slicer

---
+++ src/bot/helpers/slice.py
@@ -0,0 +1,15 @@
+def slice_queue(queue: list[str], initial_slice=0, chunk_size=100) -> list[str]:
+    """
+    Slices a queue into chunks of size `chunk_size`
+    """
+    length = len(queue)
+
+    while initial_slice < length:
+        if initial_slice + chunk_size > length:
+            chunk_size = length - initial_slice
+
+        queue_slice = queue[initial_slice : initial_slice + chunk_size]
+
+        yield queue_slice
+
+        initial_slice += chunk_size
