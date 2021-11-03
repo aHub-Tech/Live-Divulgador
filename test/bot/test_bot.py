bf03dcb1216556bf4d0b56efb2833ae0349121d7
vcwild <vcwildm@gmail.com>
2021-11-03 19:19:27 -0300

    feat: create tests for bot plugin

---
+++ test/bot/test_bot.py
@@ -0,0 +1,20 @@
+from pytest import fixture
+from src.bot.live_divulgator import LiveDivulgator
+from src.bot.plugins.twitter import TwitterPlugin
+
+
+@fixture
+def livedivulgator():
+    plugins_list = [TwitterPlugin]
+
+    bot = LiveDivulgator()
+
+    bot.add_plugins(plugins_list)
+
+    yield bot
+
+    del bot
+
+
+def test_livedivulgator_init(livedivulgator):
+    livedivulgator.run()
