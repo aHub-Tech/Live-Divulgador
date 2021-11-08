6e47604d70f26fb36683f22a7ebdabbdfa14bca7
vcwild <vcwildm@gmail.com>
2021-11-08 10:23:03 -0300

    feat: update installation scripts

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,56 +1,31 @@
 #!/bin/bash
 
-# prepare development environment to work with the project
-# execute: sh ops/scripts/develop.sh
+source $(dirname "$0")/common.sh
 
-source $(dirname "$0")/lib.sh
-
-default_dir="$(pwd)"
-venv_dir="$default_dir/venv"
-
-# terminal color
-color_default="\e[39m"
-color_error="\e[31m"
-color_ok="\e[32m"
-color_header="\e[34m"
-
-# functions
-print_header() {
-  echo
-  echo -e "--$color_header $1 $color_default"
-}
-
-print_check() {
-  status=$?
-  if [ $status -eq 0 ]
-  then
-    echo -e "[$color_ok v $color_default] $1"
-  else
-    echo -e "[$color_error x $color_default] $1"
-  fi
-}
+current_directory="$(pwd)"
+virtualenv_directory="$current_directory/venv"
 
 main() {
   print_header "Preparing python virtual environment"
 
-  python3 -m venv "$venv_dir" >/dev/null 2>&1
-  print_check "venv initialization $venv_dir"
+  python3 -m venv "$virtualenv_directory" >/dev/null 2>&1
+  print_check "virtualenv initialization $virtualenv_directory"
 
-  $venv_dir/bin/pip install --upgrade pip >/dev/null 2>&1
+  $virtualenv_directory/bin/pip install --upgrade pip >/dev/null 2>&1
   print_check "pip upgrade"
 
-  $venv_dir/bin/pip install setuptools_scm >/dev/null 2>&1
+  $virtualenv_directory/bin/pip install setuptools_scm >/dev/null 2>&1
   print_check "pip install setuptools_scm"
 
   # install local development build
-  echo -e "      ... installing livedivulgator bot component"
-  $venv_dir/bin/pip install -e .[dev] >/dev/null 2>&1
+  echo -e "       ... installing Livedivulgator bot component"
+  $virtualenv_directory/bin/pip install -e .[dev] >/dev/null 2>&1
   print_check "bot installation"
 
   # check installation
-  echo -e "      ... checking CLI"
-  $venv_dir/bin/divulgator --help >/dev/null 2>&1
-  print_check "divulgator development installation"
+  echo -e "       ... checking CLI"
+  $virtualenv_directory/bin/divulgator --help >/dev/null 2>&1
+  print_check "divulgator CLI installation"
 }
 
 # main
